b5034bcb040bb486b672f5279b0e46f99cc342e7
Fiorella-vq <fio.89@hotmail.com>
2024-09-17 21:48:38 -0300

    Generador de dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io', '.ar', '.uy'];
+let  domainHacks = ['.es'] ; //sin el pued.
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let generadorDominio = [];
+
+//sin eldomainHacks
+pronoun.forEach(start => {
+  adj.forEach(med => {
+    noun.forEach(final => {
+      extensions.forEach(exten => {
+        console.log (`${start}${med}${final}${exten}`);
+        generadorDominio.push(`${start}${med}${final}${exten}`);
+      });
+    });
+  });
+});
+
+pronoun.forEach(start => {
+  adj.forEach(med => {
+    noun.forEach(final => {
+      domainHacks.forEach(hack => {
+        let domainHacks = `${start}${med}${final.slice(0, -hack.length)}${hack}`; // tema: eliminando el array. El slice extrae parte del array que elijo. (from position, position)
+        console.log(domainHacks);
+        generadorDominio.push(domainHacks);
+      });
+    });
+  });
+});
+
+console.log(generadorDominio);
